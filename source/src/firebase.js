f9400b6d11cd6f28b5ad93899e42bc976f7bdad0
AGLanfranchi <adam.lanfranchi@gmail.com>
2022-02-24 15:41:04 +0000

    Create firebase.js

---
+++ source/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyllVTQh8arGhRqvpT-uxWkIOaCzoFbdw",
+  authDomain: "tomorrows-web-intro.firebaseapp.com",
+  projectId: "tomorrows-web-intro",
+  storageBucket: "tomorrows-web-intro.appspot.com",
+  messagingSenderId: "102740478176",
+  appId: "1:102740478176:web:443a21f63eaa056a0a2885"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
